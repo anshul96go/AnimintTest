ae30bc89070ff96ab4edc50708083030e00a4521
kferris10 <kevin.ferris10@gmail.com>
2015-03-18 23:02:49 -0600

    Add some tests of plot

--- test-mediumhard.R
+++ test-mediumhard.R
@@ -1,8 +1,8 @@
 
-library(animation)
 library(ggplot2)
 library(animint)
 library(dplyr)
+library(testthat)
 set.seed(42)
 
 brownian_data <- function(niter = 100, npoints = 10) {
@@ -28,4 +28,29 @@ a <- animint2dir(list(plot = p,
                  duration = list(iter = 250)), 
             out.dir = "brownian_motion", open.browser = FALSE)
 
+test_that("Both points and text are present", {
+  expect_equal(length(a$plots$plot$geoms), 2)
+  expect_true(substr(a$plots$plot$geoms[[1]], 7, 11) %in% c("point", "text_"))
+  expect_true(substr(a$plots$plot$geoms[[2]], 7, 11) %in% c("point", "text_"))
+})
 
+test_that("legend displays properly", {
+  # correct length
+  expect_equal(length(a$plots$plot$legend$point$entries), n_distinct(plot_dat$point))
+  # two geoms
+  expect_equal(length(a$plots$plot$legend$point$geoms), 2)
+  # correct point names
+  point_names <- sapply(a$plots$plot$legend$point$entries, function(x) x$label)
+  expect_true(all(point_names %in% unique(as.character(plot_dat$point))))
+  expect_true(all(unique(as.character(plot_dat$point)) %in% point_names))
+  # color of points matches color of text
+  expect_true(all(sapply(a$plots$plot$legend$point$entries, 
+                         function(x) x$pointcolour == x$textcolour)))
+  # distinct colors
+  color_vals <- sapply(a$plots$plot$legend$point$entries, function(x) x$pointcolour)
+  expect_equal(n_distinct(color_vals), length(color_vals))
+})
+
+test_that("proper title", {
+  expect_equal(a$plots$plot$title, "Brownian Motion in Two Dimensions")
+})
